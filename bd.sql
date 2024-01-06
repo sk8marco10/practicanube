b1a661eac1d345ce83d600c6a5842c324b63b8fc
sk8marco10 <marco_0.o@hotmail.com>
2024-01-06 17:52:49 -0500

    fourth commit

--- bd.sql
+++ bd.sql
@@ -3,8 +3,8 @@ USE bd;
 
 DROP TABLE IF EXISTS n_cliente;
 CREATE TABLE n_cliente
- (id                         VARCHAR(30)  NOT NULL,
-  nombre                       VARCHAR(20) NOT NULL,
+ (id                         VARCHAR(3)  NOT NULL,
+  nombre                       VARCHAR(30) NOT NULL,
   telefono                      VARCHAR(20) NOT NULL,
   direccion                   VARCHAR(20),
   ciudad                       VARCHAR(20),
